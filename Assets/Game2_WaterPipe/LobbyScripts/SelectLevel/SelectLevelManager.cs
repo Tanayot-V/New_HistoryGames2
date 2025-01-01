1bc3de8bfebc2d739c4f585395f9e0fc2c86d947
YamuJung <supapornthipnan@gmail.com>
2025-01-01 15:07:37 +0700

    IMG Update

--- Assets/Game2_WaterPipe/LobbyScripts/SelectLevel/SelectLevelManager.cs
+++ Assets/Game2_WaterPipe/LobbyScripts/SelectLevel/SelectLevelManager.cs
@@ -7,7 +7,6 @@ public class SelectLevelModel
 {
     public int level;
     public Vector3 originalCam;
-    public Vector3 camMoveAreaSize;
     public Sprite[] numberIMG;
 
     public SelectLevelModel(int _level, Vector3 _originalCam, Vector3 _camMoveAreaSize,Sprite[] _numberIMG)
@@ -15,7 +14,6 @@ public class SelectLevelModel
         level = _level;
         originalCam = _originalCam;
         numberIMG = _numberIMG;
-        camMoveAreaSize = new Vector3(_camMoveAreaSize.x, _camMoveAreaSize.y, 1);
     }   
 }
 
@@ -57,6 +55,5 @@ public class SelectLevelManager : MonoBehaviour
     {
          SelectLevelModel selectLevelModel = SelectLevel(currentLevel);
          mainCam.transform.position = selectLevelModel.originalCam;
-         moveAreaSR.transform.localScale = selectLevelModel.camMoveAreaSize;
     }
 }
